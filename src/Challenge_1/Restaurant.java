00bd235f1f4d082fabdfaa7c78aa0c7f77e55355
reyangga9 <reyangga5@gmail.com>
2023-09-17 17:48:06 +0700

    nambah method

--- src/Challenge_1/Restaurant.java
+++ src/Challenge_1/Restaurant.java
@@ -1,8 +1,5 @@
 package Challenge_1;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 
@@ -12,108 +9,33 @@ public class Restaurant {
         int choice;
         int hasil = 0;
         Scanner input = new Scanner(System.in);
-        List<String> restaurant = new ArrayList<>();
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("restaurant.txt", true))) {
-            // Membaca riwayat operasi dari file history.txt
-
-
-            System.out.println("================");
-            System.out.println("Selamat Datang di Nasi Goreng 99");
-            System.out.println("================");
-            System.out.println("Silahkan Pilih Makanan : ");
-            System.out.println("1.  Nasi Goreng          | 15.000");
-            System.out.println("2.  Mie Goreng           | 13.000");
-            System.out.println("3.  Bihun Goreng         | 12.000");
-            System.out.println("4.  Es Teh Manis         | 3.000");
-            System.out.println("5.  Es Jeruk             | 8.000");
-            System.out.println("98. Liat Menu");
-            System.out.println("99. Pesan dan Bayar");
-
+            menu();
 
             for(int i =0;i<99999;i++){
 
-                System.out.print("=======Pilih Menu=======  : ");
+                System.out.print("=======Pilih Menu=======: ");
                 choice = input.nextInt();
 
                 switch (choice) {
                     case 1:
-                        System.out.println("Anda memesan nasi goreng ");
-                        System.out.print("Berapa banyak anda ingin memesan : ");
-                        many = input.nextInt();
-                        if(many == 0){
-                            break;
-                        } else{
-                            hasil += (15000 * many);
-                            restaurant.add("Nasi Goreng " + many);
-                            System.out.println("Total harga yang harus dibayar adalah " + hasil);
-
-                        }
-
+                        Pesanan(hasil,15000,"Nasi Goreng");
                         break;
                     case 2:
-                        System.out.println("Anda memesan mie goreng ");
-                        System.out.print("Berapa banyak anda ingin memesan : ");
-                        many = input.nextInt();
-                        if(many == 0){
-                            break;
-                        } else{
-                            hasil += (13000 * many);
-                            System.out.println("Total harga yang harus dibayar adalah " + hasil);
-
-
-                        }
-
+                        Pesanan(hasil,13000,"Mie Goreng");
                         break;
                     case 3:
-                        System.out.println("Anda memesan bihun goreng ");
-                        System.out.print("Berapa banyak anda ingin memesan : ");
-                        many = input.nextInt();
-                        if(many == 0){
-                            break;
-                        } else{
-                            hasil += (12000 * many);
-                            System.out.println("Total harga yang harus dibayar adalah " + hasil);
-
-                        }
+                        Pesanan(hasil,12000,"Bihun Goreng");
                         break;
                     case 4:
-                        System.out.println("Anda memesan es teh manis ");
-                        System.out.print("Berapa banyak anda ingin memesan : ");
-                        many = input.nextInt();
-                        if(many == 0){
-                            break;
-                        } else{
-                            hasil += (3000 * many);
-                            System.out.println("Total harga yang harus dibayar adalah " + hasil);
-                        }
-
+                        Pesanan(hasil,3000,"Es Teh Manis");
                         break;
                     case 5:
-                        System.out.println("Anda memesan es jeruk ");
-                        System.out.print("Berapa banyak anda ingin memesan : ");
-                        many = input.nextInt();
-                        if(many == 0){
-                            break;
-                        } else{
-                            hasil += (8000 * many);
-                            System.out.println("Total harga yang harus dibayar adalah " + hasil);
-                        }
 
-
+                        Pesanan(hasil,8000,"Es Jeruk");
                         break;
                     case 98:
-                        System.out.println("================");
-                        System.out.println("Selamat Datang di Nasi Goreng 99");
-                        System.out.println("================");
-                        System.out.println("Silahkan Pilih Makanan : ");
-                        System.out.println("1.  Nasi Goreng          | 15.000");
-                        System.out.println("2.  Mie Goreng           | 13.000");
-                        System.out.println("3.  Bihun Goreng         | 12.000");
-                        System.out.println("4.  Es Teh Manis         | 3.000");
-                        System.out.println("5.  Es Jeruk             | 8.000");
-                        System.out.println("98. Liat Menu");
-                        System.out.println("99. Pesan dan Bayar");
+                       menu();
                         break;
                     case 99:
                         System.out.println("Total biaya pesanan anda adalah : " + hasil);
@@ -128,17 +50,40 @@ public class Restaurant {
                         break;
 
                 }
-            }
-
-
-
-        }catch (IOException e) {
-            System.out.println("Gagal menulis ke file restaurant.txt.");
         }
 
-
-
-
-
     }
+    static void menu(){
+        System.out.println("================");
+        System.out.println("Selamat Datang di  Binar FUD");
+        System.out.println("================");
+        System.out.println("Silahkan Pilih Makanan : ");
+        System.out.println("1.  Nasi Goreng          | 15.000");
+        System.out.println("2.  Mie Goreng           | 13.000");
+        System.out.println("3.  Bihun Goreng         | 12.000");
+        System.out.println("4.  Es Teh Manis         | 3.000");
+        System.out.println("5.  Es Jeruk             | 8.000");
+        System.out.println("98. Liat Menu");
+        System.out.println("99. Pesan dan Bayar");
+    }
+
+    static int Pesanan(int hasil,int harga, String pesanan){
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Anda memesan " + pesanan);
+        int banyak = input.nextInt();
+        System.out.print("Berapa banyak anda ingin memesan : ");
+
+        if(banyak == 0){
+          return 0;
+        } else{
+             hasil += (harga * banyak);
+            System.out.println("Total harga yang harus dibayar adalah " + hasil);
+            return hasil;
+
+        }
+    }
+
+
 }
